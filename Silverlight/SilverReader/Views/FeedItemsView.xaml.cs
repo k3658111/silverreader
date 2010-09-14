938c8abf0fddc7da9dfad7a8429207661d60353e
wangxuesong <wangxuesong@thtf.com.cn>
2010-09-14 15:49:26 +0800

    add FeedsView & FeedItemsView by Blend

---
+++ Silverlight/SilverReader/Views/FeedItemsView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SilverReader
+{
+	/// <summary>
+	/// Interaction logic for FeedItemsView.xaml
+	/// </summary>
+	public partial class FeedItemsView : UserControl
+	{
+		public FeedItemsView()
+		{
+			this.InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+		}
+	}
+}
