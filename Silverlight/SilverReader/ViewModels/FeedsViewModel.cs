0a572ed3f216f14949c93ee615e272aee2b831c9
wangxuesong <wangxuesong@thtf.com.cn>
2010-09-14 23:57:24 +0800

    添加 AddFeedCommand

--- Silverlight/SilverReader/ViewModels/FeedsViewModel.cs
+++ Silverlight/SilverReader/ViewModels/FeedsViewModel.cs
@@ -7,16 +7,54 @@ using System.ComponentModel;
 namespace SilverReader
 {
     using System.Collections.ObjectModel;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
     using System.ServiceModel.Syndication;
+    using System.Windows.Input;
+    using System.Xml;
+    using Microsoft.Expression.Interactivity.Core;
 
     public class FeedsViewModel : INotifyPropertyChanged
     {
         public FeedsViewModel()
         {
-            
+            Feeds = new ObservableCollection<SyndicationFeed>();
+            AddFeedCommand = new ActionCommand(delegate(object o)
+                                                   {
+                                                       string url = (string) o;
+                                                       Uri feedUri;
+                                                       Uri.TryCreate(url, UriKind.Absolute, out feedUri);
+                                                       if (feedUri == null)
+                                                           return;
+                                                       WebClient request = new WebClient();
+                                                       request.DownloadStringCompleted +=
+                                                           delegate(object sender, DownloadStringCompletedEventArgs e)
+                                                           {
+                                                               if (e.Error != null)
+                                                                   return;
+
+                                                               string xml = e.Result;
+                                                               if (xml.Length == 0)
+                                                                   return;
+
+                                                               StringReader stringReader = new StringReader(xml);
+                                                               XmlReader reader = XmlReader.Create(stringReader);
+                                                               SyndicationFeed feed = SyndicationFeed.Load(reader);
+
+                                                               if (Feeds.Where(f => f.Title.Text == feed.Title.Text).ToList().Count > 0)
+                                                                   return;
+
+                                                               Feeds.Add(feed);
+
+                                                           };
+                                                       request.DownloadStringAsync(feedUri);
+
+                                                   });
         }
 
         public ObservableCollection<SyndicationFeed> Feeds { get; private set; }
+        public ICommand AddFeedCommand { get; private set; }
 
         #region INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
